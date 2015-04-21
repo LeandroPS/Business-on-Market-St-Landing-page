e233b272248a21330a0d22fdc7108eed32186586
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-04-21 13:31:43 -0400

    Changing the way that the service-canvas will show the images

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,9 +1,38 @@
-var Photos = ["../Images/Canvas_Background/research.jpg","Images/Canvas_Background/brand.jpg","Images/Canvas_Background/web_development.jpg","Images/Canvas_Background/business.jpg","Images/Canvas_Background/marketing.jpg"];
+var Photos = ["Images/Canvas_Background/research.jpg","Images/Canvas_Background/brand.jpg","Images/Canvas_Background/web_development.jpg","Images/Canvas_Background/business.jpg","Images/Canvas_Background/marketing.jpg"];
+
+function next(){
+	var este = $("div.services ul li.current");
+	indx = este.index();
+	
+	if(este.is(":last-child")){
+		var next = $("div.services ul li:first-child");
+	}else{
+		var next = este.next();
+	}
+	
+	$("div.services-canvas").css("background-image", "url("+Photos[next.index()]+")");
+	este.removeClass("current");
+	next.addClass("current");
+}
+
 
 $(function(){
+	
+	//setTimeout(next(), 2000);
+	
 	$("div.services ul li").click(function(){
 		indx = $(this).index();
-		//alert(Photos[indx]);
-		$("div.services-canvas").css("background-color", "green");
+
+		
+		/*
+		$("div.services-canvas").css("background-image", "url("+Photos[indx]+")");
+		*/
+		
+		$("div.services-canvas img").removeClass("current");
+		$("div.services-canvas img:eq("+indx+")").addClass("current");
+		///
+		
+		$("div.services ul li").removeClass("current");
+		$(this).addClass("current");
 	});
 });
