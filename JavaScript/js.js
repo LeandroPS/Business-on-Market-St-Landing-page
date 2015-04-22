16fdcef2ca91e980c589b09edba2010dabf4d648
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-04-22 01:33:29 -0400

    changing the services button behavior

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -2,33 +2,40 @@ var Photos = ["Images/Canvas_Background/research.jpg","Images/Canvas_Background/
 
 var Quotes = ["<span class='strong'>Build</span> <span class='center'>your Business on a Solid Rock Foundation</span>", "Get your brand <span class='strong'>moving</span>", "Refresh your <span class='strong'>online</span> <span class='right'>presence</span>", "Secure new <span class='strong'>streams of revenue</span>", "Increase your <span class='strong'>revenue</span> <span class='right'>year by year</span>"];
 
+var timer;
+
 function next(){
 	var este = $("div.services ul li.current");
 	indx = este.index();
 	
 	if(este.is(":last-child")){
-		var next = $("div.services ul li:first-child");
+		var nextt = $("div.services ul li:first-child");
 	}else{
-		var next = este.next();
+		var nextt = este.next();
 	}
 	
-	$("div.services-canvas").css("background-image", "url("+Photos[next.index()]+")");
+	$("div.services-canvas img").removeClass("current");
+	$("div.services-canvas img:eq("+nextt.index()+")").addClass("current");
+	$("div.services-canvas div.service-comment p").html(Quotes[nextt.index()]);
 	este.removeClass("current");
-	next.addClass("current");
+	nextt.addClass("current");
+	timer = setTimeout(next, 4000);
+}
+
+function stop(){
+	clearTimeout(timer);
+	timer = 0;
 }
 
 
 $(function(){
 	
-	//setTimeout(next(), 2000);
+	next();
 	
 	$("div.services ul li").click(function(){
-		indx = $(this).index();
-
+		stop();
 		
-		/*
-		$("div.services-canvas").css("background-image", "url("+Photos[indx]+")");
-		*/
+		indx = $(this).index();
 		
 		$("div.services-canvas img").removeClass("current");
 		$("div.services-canvas img:eq("+indx+")").addClass("current");
